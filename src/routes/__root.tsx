741e94c17985c74d3b62c184e99da1b8dddbde28
Nils Hartmann <nils@nilshartmann.net>
2024-03-04 16:33:59 +0100

    fix route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import {TanStackRouterDevtools} from "@tanstack/router-devtools";
-
 export const Route = createRootRoute({
   component: GlobalPageLayout,
 });
@@ -11,20 +10,20 @@ export default function GlobalPageLayout() {
         <Outlet />
       <div>
         <ul>
-          <li><Link to={"/"}>Home</Link></li>
-          <li><Link to={"/a"}>A (should point to index route /a)</Link></li>
-          <li><Link to={"/a/"}>A Slash (should also point to index route /a)</Link></li>
-          <li><Link to={"/n"}>N (route.tsx only)</Link></li>
-          <li><Link to={"/n/nn"}>N/NN <span className={"text-red-400"}>Goes to /n component?</span> </Link></li>
-          <li><Link to={"/x"}>/X (index.tsx only)</Link></li>
-          <li><Link to={"/x/y"}>/X/Y <span className={"text-red-400"}>not nested in /x, as /x is index.tsx only</span>(</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/"}>Home</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a"}>A (should point to index route /a)</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/"}>A Slash (should also point to index route /a)</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/n"}>N <span className={"text-red-400"}>(route.tsx only, but can be accessed as index route for /n?)</span></Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/n/nn"}>N/NN </Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/x"}>/X (index.tsx only)</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/x/y"}>/X/Y <span className={"text-red-400"}>not nested in /x, as /x is index.tsx only</span>(</Link></li>
 
-          <li><Link to={"/a/b"}>A/B</Link></li>
-          <li><Link to={"/a/aa"}>A/AA (within Layout Route _c)</Link></li>
-          <li><Link to={"/a/bb"}>A/BB (within Layout Route _c)</Link></li>
-          <li><Link to={"/a/p1"}>A/P1 within route group "(d)"</Link></li>
-          <li><Link to={"/a/fff"}>A/FFF within route group "(d)"</Link></li>
-          <li><Link to={"/a/fff/ggg"}>A/FFF/GGG within route group "(d)"</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/b"}>A/B</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/aa"}>A/AA (within Layout Route _c)</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/bb"}>A/BB (within Layout Route _c)</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/p1"}>A/P1 within route group "(d)"</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/fff"}>A/FFF within route group "(d)"</Link></li>
+          <li><Link className={"cursor-pointer underline text-blue-600"} to={"/a/fff/ggg"}>A/FFF/GGG within route group "(d)"</Link></li>
         </ul>
       </div>
       <TanStackRouterDevtools/>
--- src/routes/n/route.tsx
+++ src/routes/n/route.tsx
@@ -1,5 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {createFileRoute, Outlet} from '@tanstack/react-router'
 
 export const Route = createFileRoute('/n')({
-  component: () => <div className={"bg-amber-200 p-8"}>Hello Index + Layout /n!</div>
+  component: () => <div className={"bg-amber-200 p-8"}>
+    Hello Index + Layout /n!
+  <Outlet />
+  </div>
 })
